7a46e3c324b017b1c26498127c7f583bdc177826
tayinde <tbiayinde@gmail.com>
2021-03-21 15:24:26 -0400

    directory reference fixes, menu code

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+var onHomePage = location.pathname == "/" || location.pathname.indexOf("index") > -1;
+var onLocalComputer = location.pathname.indexOf(".html") > -1;
+function section(id) {
+    if (onHomePage) {
+        document.getElementById(id).scrollIntoView();
+    } else {
+        location.href = onLocalComputer ? `./index.html#${id}` : './home' ;
+    }
+}
