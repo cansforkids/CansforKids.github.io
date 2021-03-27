e28a220a49e72692d60f958813eac89571f88758
tayinde <tbiayinde@gmail.com>
2021-03-27 18:05:21 -0400

    mobile friendliness for home page, partially done for some other pages but i'll do that a bit later

--- js/menu.js
+++ js/menu.js
@@ -7,3 +7,24 @@ function section(id) {
         location.href = onLocalComputer ? `./index.html#${id}` : '/#' + id ;
     }
 }
+
+var menuIsOpen = false;
+
+function openMenu() {
+    $('#menu').css('height', '100vh');
+    $('#menu a').eq(1).css('margin-top', '100px')
+    $('#menu a, #menu button').css('display', 'block')
+    $('#menu-button').attr('href', 'javascript:closeMenu()');
+    menuIsOpen = true;
+}
+
+$(window).click((event) => {
+    if (menuIsOpen && event.pageY > $('#menu').offset().top + 70) closeMenu();
+})
+
+function closeMenu() {
+    $('#menu').css('height', '70px')
+    $('#menu a, #menu button').css('display', 'none')
+    $('#menu-button').attr('href', 'javascript:openMenu()');
+    menuIsOpen = false;
+}
